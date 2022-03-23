3f51abe84700634682fe9d6b7153ee93c480d160
dickyadrianto <dickyadrianto@gmail.com>
2022-03-24 00:46:54 +0800

    update

---
+++ resources/views/customer/order.blade.php
@@ -0,0 +1,60 @@
+@extends('template')
+
+@section('main-content')
+    @include('layouts.navbars.navbar')
+    <!-- Header -->
+    <div class="header bg-gradient-primary">
+        <div class="container-fluid">
+            <div class="header-body">
+                <div class="row align-items-center py-4">
+                    <div class="col-lg-6 col-7">
+                        {{--                        <h6 class="h2 text-white d-inline-block mb-0">Customer</h6>--}}
+                        <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+                            <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                                <li class="breadcrumb-item"><a href="http://127.0.0.1:8000/welcome"><i class="fas fa-home"></i></a></li>
+                                <li class="breadcrumb-item"><a href="{{ route('customer-order') }}">Order</a></li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <div class="col-lg-6 col-5 text-right">
+                        <a href="#" class="btn btn-sm btn-neutral">Filters</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid pt-3">
+        <div class="header-body">
+            <!-- Card stats -->
+            <div class="row">
+                <div class="col-xl-12 col-lg-12">
+                    <div class="card card-stats mb-4 mb-xl-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-auto">
+                                    <img class="img-center userImg150" src="{{ asset('argon/argon/img/theme/harness.jpg') }}">
+                                </div>
+                                <div class="col">
+                                    <h5 class="card-title text-uppercase h2 mb-0">Herness Orange</h5>
+                                    <span class="h2 font-weight-bold mb-0">350,897</span>
+                                </div>
+                                <div class="col-auto">
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Status</h5>
+                                    <div class="bg-success rounded">
+                                        <span class="h2 text-white p-1">Deliverd</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="mt-3 mb-0 text-muted text-sm">
+                                <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                <span class="text-nowrap">Since last month</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
